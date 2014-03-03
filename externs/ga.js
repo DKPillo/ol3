70b4e8c18a8f3b7c91b37bbdb9f230aca7f85bb3
loicgasser <loicgasser4@gmail.com>
2014-03-03 15:37:48 +0100

    Add apiUrl as part of ga namespace

--- externs/ga.js
+++ externs/ga.js
@@ -5,4 +5,5 @@
 
 
 function getConfig(){};
+function getApiUrl(){};
 window.GeoAdmin;
--- src/ga/ga.js
+++ src/ga/ga.js
@@ -1,1 +1,4 @@
 goog.provide('ga');
+goog.provide('ga.apiUrl')
+
+ga.apiUrl = getApiUrl() || '//api3.geo.admin.ch';
